23f4ce210c49ce738a2fc36d625613b31716c120
saptahadiwijaya7 <saptahadiwijaya7@gmail.com>
2022-08-05 16:32:46 +0700

    rev link

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -126,7 +126,7 @@ export default function Home() {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="#"
+                    href="https://www.figma.com/file/rAkaOzei2imDzX7SJuotIp/Untitled?node-id=0%3A1"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-400 hover:bg-orange-500 md:py-4 md:text-lg md:px-10"
                   >
                     Figma
